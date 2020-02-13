aa9ebde6bb5cba2b54fa079f1b80c7fdff5a7ce5
hhhrrrttt222111 <43471295+hhhrrrttt222111@users.noreply.github.com>
2020-02-13 18:28:11 +0700

    Add files via upload

---
+++ TIC-TAC-TOE/lib/dialog.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class CustomDialog extends StatelessWidget {
+  final title;
+  final content;
+  final VoidCallback callback;
+  final actionText;
+
+  CustomDialog(this.title, this.content, this.callback, [this.actionText = "Reset"]);
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text(title),
+      content: Text(content),
+      actions: <Widget>[
+        FlatButton(onPressed: callback, child: Text(actionText), color: Colors.white,)
+      ],
+    );
+  }
+}
